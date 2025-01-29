99fdac86c68bbe7f0e7cf40bdc968f5cf1999404
marcoscosta2602 <marcos.costa@mvc.digital>
2025-01-29 20:15:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from BancoDados import get_connection, close_connection, list_schemas
+from BancoDados import get_connection, close_connection, list_schemas, get_sales_data
 import streamlit as st
 
 
 conn = get_connection()
 
 Segmento = list(list_schemas(conn))
-print(Segmento) 
-
-if conn:
-    close_connection(conn)
 
 
-st.selectbox('Escolha o segmento' + Segmento)
+st.title("📊 Dashboard Empresarial")
+
+# Selectbox para o usuário escolher um segmento
+if Segmento:
+    segmento_escolhido = st.selectbox('Escolha o segmento:', Segmento)
+
+    dados = get_sales_data(segmento_escolhido)
+
+    # Exibir os dados se houver resultado
+    if not dados.empty:
+        st.write(f"📈 Receita total por data para o schema: **{segmento_escolhido}**")
+        st.bar_chart(dados.set_index("data_venda"))  # Usa "data_venda" como eixo X
+    else:
+        st.warning("Nenhum dado encontrado para esse schema.")
+else:
+    st.error("Nenhum schema encontrado no banco.")
