26cf179c2391b78bf958209091a322efefa9b73a
marcoscosta2602 <marcos.costa@mvc.digital>
2025-01-29 12:49:21 -0300

    Ajuste app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from BancoDados import get_connection, close_connection
+from BancoDados import get_connection, close_connection, list_schemas
+import streamlit_app as st
+
 
 conn = get_connection()
+
+Segmento = list(list_schemas(conn))
+print(Segmento)
+
 if conn:
     close_connection(conn)
-
